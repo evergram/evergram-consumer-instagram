251887a2820ac884fa13ce432ac5a78a1a078c58
crimsonronin <josh.stuart@zoopcommerce.com>
2015-03-28 20:18:19 +1100

    A commit so I have a record of a bigger back filling method.

--- app/consumer/index.js
+++ app/consumer/index.js
@@ -3,14 +3,15 @@
  */
 
 var q = require('q');
+var _ = require('lodash');
 var moment = require('moment');
+var config = require('../config');
 var common = require('evergram-common');
 var logger = common.utils.logger;
 var aws = common.aws;
-var config = require('../config');
-var instagram = common.instagram;
-var print = common.print;
-var User = common.models.User;
+var instagramManager = common.instagram.manager;
+var printManager = common.print.manager;
+var userManager = common.user.manager;
 
 /**
  * A consumer that handles all of the consumers
@@ -38,62 +39,24 @@ Consumer.prototype.consume = function () {
             /**
              * Find the user
              */
-            User.findOne({'_id': id}, function (err, user) {
+            userManager.find({criteria: {'_id': id}}).then((function (user) {
                 if (user != null) {
-                    var userPrintableImageSet;
                     var dateRun = new Date();
 
                     /**
-                     * Getting an existing printable image set, or create a new one.
+                     * process any previous image sets that haven't been closed out
                      */
-                    print.manager.findCurrentByUser(user)
-                    .then(function (printableImageSet) {
-                        if (printableImageSet == null) {
-                            userPrintableImageSet = print.manager.getNewPrintableImageSet(user);
-                        } else {
-                            userPrintableImageSet = printableImageSet;
-                        }
-
-                        logger.info('Getting printable images for: ' + user.instagram.username);
-
+                    this.processReadyForPrintImageSet(user).then((function () {
                         /**
-                         * If we are a new user we won't put any date restrictions on the query
+                         * process the current images
                          */
-                        if (user.isInFirstPeriod()) {
-                            return instagram.manager.findPrintableImagesByUser(user);
-                        } else {
-                            return instagram.manager.findPrintableImagesByUser(user, userPrintableImageSet.date);
-                        }
-                    }, function (err) {
-                        logger.error('There was an error when finding printable image set for ' + user.instagram.username, err);
-
-                        resolve();
-                    })
+                        return this.processCurrentImageSet(user);
+                    }).bind(this))
                     /**
-                     * Get the printable images for the user and add them to the printable set
+                     * Delete the message from the queue
                      */
-                    .then(function (images) {
-                        logger.info('Found ' + images.length + ' images for: ' + user.instagram.username);
-
-                        //add the new images
-                        userPrintableImageSet.addImages('instagram', images);
-                        //save to db
-                        return print.manager.save(userPrintableImageSet);
-                    }, function (err) {
-                        logger.error('There was an error when finding images for ' + user.instagram.username, err);
-                        resolve();
-                    })
-                    /**
-                     * Remove the message from the queue.
-                     */
-                    .then(function (printableImageSet) {
-                        logger.info('Saved images for: ' + user.instagram.username);
-
-                        return deleteMessageFromQueue(message);
-                    }, function (err) {
-                        logger.error('There was an error when deleting from the queue for ' + user.instagram.username, err);
-
-                        resolve();
+                    .then(function () {
+                        return deleteMessageFromQueue(results[0]);
                     })
                     /**
                      * Save the user with a new last run and in queue state.
@@ -110,7 +73,7 @@ Consumer.prototype.consume = function () {
                 } else {
                     deleteMessageFromQueue(results[0]).then(resolve);
                 }
-            });
+            }).bind(this));
         } else {
             logger.info('No messages on queue');
             resolve();
@@ -126,10 +89,184 @@ Consumer.prototype.consume = function () {
     return deferred.promise;
 };
 
+/**
+ * This is the current period image set
+ */
+Consumer.prototype.processCurrentImageSet = function (user) {
+    return printManager.findCurrentByUser(user)
+    .then(function (printableImageSet) {
+        if (printableImageSet == null) {
+            printableImageSet = printManager.getNewPrintableImageSet(user);
+        }
+
+        logger.info('Getting current printable images for: ' + user.instagram.username);
+
+        return processPrintableImageSet(user, printableImageSet);
+    });
+}
+
+/**
+ * These are image sets that are past their period, but have not
+ * yet been marked as "ready for print"
+ *
+ * //TODO refactor this beast because it is only really needed due to legacy users.
+ */
+Consumer.prototype.processReadyForPrintImageSet = function (user) {
+    var deferred = q.defer();
+
+    var numberOfPeriods = user.getCurrentPeriod();
+    if (numberOfPeriods > 0) {
+        printManager.findAllPreviousNotReadyForPrintByUser(user)
+        .then((function (imageSets) {
+            var imageDeferreds = [];
+
+            if (!!imageSets && imageSets.length > 0) {
+                logger.info('Getting previous ready for print images for: ' + user.instagram.username);
+
+                /**
+                 * We do one final fetch on images to make sure we haven't missed any
+                 * and then set ready for print.
+                 */
+                _.forEach(imageSets, (function (imageSet) {
+                    var imageDeferred = q.defer();
+                    imageDeferreds.push(imageDeferred.promise);
+
+                    imageSet.isReadyForPrint = true;
+                    this.processPrintableImageSet(user, imageSet).then(function () {
+                        imageDeferred.resolve();
+                    });
+                }).bind(this));
+
+                q.all(imageDeferreds).then(deferred.resolve);
+            } else {
+                /**
+                 * We get all previous sets to find the ones that are missing.
+                 */
+                var printedImageSets = printManager.findAllPrintedByUser(user)
+                .then((function (imageSets) {
+                    logger.info('Backfilling print images for: ' + user.instagram.username);
+
+                    /**
+                     * Check to see if each period is already printed.
+                     * If not, it is missing and we will create a new set.
+                     */
+                    _.forEach(new Array(numberOfPeriods), (function (el, i) {
+                        if (imageSets.length == 0 || !printableImageSetsContainPeriod(user, imageSets, i)) {
+                            var imageDeferred = q.defer();
+                            imageDeferreds.push(imageDeferred.promise);
+
+                            var imageSet = printManager.getNewPrintableImageSet(user, i);
+                            imageSet.isReadyForPrint = true;
+                            /**
+                             * If it's not last months, we will assume it's already printed.
+                             */
+                            if (isPreviousPrintableImageSet(user, imageSet)) {
+                                imageSet.isPrinted = false;
+                            } else {
+                                imageSet.isPrinted = true;
+                            }
+                            this.processPrintableImageSet(user, imageSet).then(function () {
+                                imageDeferred.resolve();
+                            });
+                        }
+                    }).bind(this));
+
+                    q.all(imageDeferreds).then(function () {
+                        logger.info("Completed ready for print");
+
+                        deferred.resolve();
+                    });
+                }).bind(this));
+            }
+        }).bind(this));
+    } else {
+        deferred.resolve();
+    }
+
+    return deferred.promise;
+};
+
+/**
+ * Finds and saves images for the passed user and image set.
+ *
+ * @param user
+ * @param printableImageSet
+ * @returns {*}
+ */
+Consumer.prototype.processPrintableImageSet = function (user, printableImageSet) {
+    var printableImagesPromise;
+
+    /**
+     * If we are a new user we won't put any date restrictions on the query
+     */
+    if (user.isInFirstPeriod()) {
+        printableImagesPromise = instagramManager
+        .findPrintableImagesByUser(user, null, printableImageSet.endDate);
+    } else {
+        printableImagesPromise = instagramManager
+        .findPrintableImagesByUser(user, printableImageSet.startDate, printableImageSet.endDate);
+    }
+
+    return printableImagesPromise
+    /**
+     * Get the printable images for the user and add them to the printable set
+     */
+    .then(function (images) {
+        logger.info('Found ' + images.length + ' images for: ' + user.instagram.username);
+
+        /**
+         * Add the new images.
+         */
+        printableImageSet.addImages('instagram', images);
+
+        /**
+         * Save to db.
+         */
+        return printManager.save(printableImageSet);
+    }, function (err) {
+        logger.error('There was an error when finding images for ' + user.instagram.username, err);
+        resolve();
+    })
+};
+
+/**
+ * Is the passed image set date the same as the previous user date
+ * @param user
+ * @param imageSet
+ * @returns {*}
+ */
+function isPreviousPrintableImageSet(user, imageSet) {
+    return moment(imageSet.startDate).isSame(user.getPreviousPeriodStartDate(1));
+}
+
+/**
+ *
+ * @param imageSets
+ * @param period
+ * @returns {boolean}
+ */
+function printableImageSetsContainPeriod(user, imageSets, period) {
+    return _.some(imageSets, function (imgSet) {
+        return user.getPeriodFromStartDate(imgSet.startDate) == period;
+    });
+}
+
+/**
+ * Gets the next run date based on the date the process was run.
+ *
+ * @param dateRun
+ * @returns {Date}
+ */
 function getNextRunDate(dateRun) {
     return new Date(moment(dateRun).add(config.userNextRunDelay, 'seconds'));
 }
 
+/**
+ * Convenience function to delete a message from the SQS.
+ *
+ * @param result
+ * @returns {*}
+ */
 function deleteMessageFromQueue(result) {
     return aws.sqs.deleteMessage(aws.sqs.QUEUES.INSTAGRAM, result);
 }
