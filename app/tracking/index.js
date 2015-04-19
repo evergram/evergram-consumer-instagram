f753cce4041b4964883f05b56461f0f73f51f8f5
crimsonronin <josh.stuart@zoopcommerce.com>
2015-04-19 15:56:17 +1000

    Added TZ UTC Fixed tracking format to segment Created full backfill script.

--- app/tracking/index.js
+++ app/tracking/index.js
@@ -3,6 +3,8 @@
  */
 
 var _ = require('lodash');
+var q = require('q');
+var moment = require('moment');
 var common = require('evergram-common');
 var logger = common.utils.logger;
 var trackingManager = common.tracking.manager;
@@ -24,48 +26,30 @@ function TrackingManager() {
  * @param images
  */
 TrackingManager.prototype.trackTaggedImages = function (user, imageSet, images) {
-    var event = 'Tagged Images';
-    var total = 0;
-    var owned = 0;
-    var other = 0;
+    var deferreds = [];
+    var event = 'Tagged a photo';
+
+    logger.info('Tracking ' + event + ' for ' + user.getUsername());
 
     _.forEach(images, function (image) {
         if (!imageSet.containsImage('instagram', image)) {
-            total++;
-            if (image.isOwner) {
-                owned++;
-            } else {
-                other++;
-            }
+            var deferred = trackingManager.trackEvent(user, event, {
+                service: 'instagram',
+                owner: image.owner,
+                type: image.isOwner ? 'own' : 'friends',
+                isHistorical: moment(image.createdOn).isBefore(user.createdOn),
+                link: image.metadata.link,
+                image: image.raw,
+                period: user.getPeriodFromStartDate(imageSet.startDate),
+                createdOn: moment(image.createdOn).toDate(),
+                taggedOn: moment(image.taggedOn).toDate()
+            }, image.taggedOn);
+
+            deferreds.push(deferred);
         }
     });
 
-    if (total > 0) {
-        logger.info('Tracking ' + event + ' for ' + user.getUsername());
-
-        return trackingManager.incrementMultiple(user, {
-            'Total Images Tagged': total,
-            'Total Own Images Tagged': owned,
-            'Total Other Images Tagged': other
-        }).then(function () {
-            return trackingManager.trackEvent(user, event, {
-                'Service': 'Instagram',
-                'Plan': user.billing.option,
-                'Instagram Username': user.instagram.username,
-                'Period': user.getPeriodFromStartDate(imageSet.startDate),
-                'Country': imageSet.user.address.country,
-                'State': imageSet.user.address.state,
-                'City': imageSet.user.address.suburb,
-                'Image Set Start Date': imageSet.startDate,
-                'Image Set End Date': imageSet.endDate,
-                'Total Images Tagged': total,
-                'Total Own Images Tagged': owned,
-                'Total Other Images Tagged': other
-            });
-        });
-    } else {
-        return;
-    }
+    return q.all(deferreds);
 };
 
 /**
---
+++ bin/backfill.js
@@ -0,0 +1,126 @@
+/**
+ * Module dependencies
+ */
+process.env.TZ = 'UTC';
+
+var q = require('q');
+var _ = require('lodash');
+var moment = require('moment');
+var common = require('evergram-common');
+var logger = common.utils.logger;
+var userManager = common.user.manager;
+var consumer = require('../app/consumer');
+var printManager = common.print.manager;
+
+//init db
+common.db.connect();
+
+//var options = {criteria: {'instagram.username': 'jacq1313'}};
+var options = {};
+
+//backfill
+userManager.findAll(options).then(function (users) {
+    var processUsers = [];
+    _.forEach(users, function (user) {
+        processUsers.push(user);
+    });
+
+    var current = 0;
+    var process = function () {
+        logger.info('Processing ' + current);
+        processImagesSetsForUser(processUsers[current]).then(function () {
+            logger.info('Done processing ' + current);
+            current++;
+            if (current <= processUsers.length) {
+                process();
+            }
+        });
+    };
+    process();
+});
+
+function processImagesSetsForUser(user) {
+    var deferreds = [];
+    var numberOfPeriods = user.getCurrentPeriod();
+    logger.info(user.getUsername() + ' has ' + numberOfPeriods + ' periods');
+
+    if (numberOfPeriods > 0) {
+        var periods = new Array(numberOfPeriods + 1);
+
+        _.forEach(periods, function (period, i) {
+            var deferred = q.defer();
+            deferreds.push(deferred.promise);
+
+            processImageSet(user, i).then(function () {
+                deferred.resolve();
+            });
+        });
+    }
+
+    return q.all(deferreds);
+}
+
+function processImageSet(user, i) {
+    var deferred = q.defer();
+
+    var startDate = moment(user.getPeriodStartDate(i));
+    var startDate1 = moment(startDate).subtract(1, 'days');
+    var startDate2 = moment(startDate).add(1, 'days');
+
+    logger.info('Checking ' + i + ' period ' + startDate1.format() + ' : ' + startDate2.format());
+
+    printManager.find({
+        criteria: {
+            'user._id': user._id.toString(),
+            'startDate': {
+                '$gte': startDate1.toDate(),
+                '$lt': startDate2.toDate()
+            }
+        }
+    }).
+    then(function (imageSet) {
+        if (!!imageSet) {
+            logger.info('Image set already exists for period ' + i + ' for ' + user.getUsername());
+            imageSet.period = i;
+            imageSet.save(function (err) {
+                if (!!err) {
+                    logger.error(err);
+                } else {
+                    logger.info('We are done updating the period with ' + i);
+                }
+                deferred.resolve();
+            });
+        } else {
+            logger.info('Getting new image set for period ' + i + ' for ' + user.getUsername());
+            var newImageSet = printManager.getNewPrintableImageSet(user, i);
+
+            consumer.processPrintableImageSet(user, newImageSet).
+            then(function () {
+                if (newImageSet.period != user.getCurrentPeriod()) {
+                    newImageSet.isPrinted = true;
+                    newImageSet.isReadyForPrint = true;
+                    newImageSet.save(function (err) {
+                        if (!!err) {
+                            logger.error(err);
+                        } else {
+                            logger.info('Completed new image set for period ' + i + ' for ' + user.getUsername());
+                        }
+                        deferred.resolve();
+                    });
+                }
+            }).
+            fail(function (err) {
+                logger.error(err);
+                deferred.resolve();
+            }).
+            done();
+        }
+    }).
+    fail(function (err) {
+        logger.error(err);
+        deferred.resolve();
+    }).
+    done();
+
+    return deferred.promise;
+}
