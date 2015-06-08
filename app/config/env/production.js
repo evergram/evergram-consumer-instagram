ed62682d2af32dc1e2727cac3e71dc0ebd474a50
crimsonronin <josh.stuart@zoopcommerce.com>
2015-06-08 15:17:37 +1000

    Now using slipstream for queue management.

--- app/config/env/production.js
+++ app/config/env/production.js
@@ -4,14 +4,12 @@
  */
 
 module.exports = {
-    //seconds, 6hrs default
-    userNextRunDelay: process.env.USER_NEXT_RUN_DELAY || 60 * 60 * 6,
+    //seconds, 10mins default
+    userNextRunDelay: process.env.USER_NEXT_RUN_DELAY || 600,
     sqs: {
         //seconds
-        waitTime: process.env.SQS_WAIT_TIME || 20
+        waitTime: process.env.SQS_WAIT_TIME || 20,
+        visibilityTime: process.env.SQS_VISIBILITY_TIME || 20
     },
-
-    //seconds
-    retryWaitTime: process.env.RETRY_WAIT_TIME || 60,
     track: process.env.TRACK_TAGGING || true
 };
--- app/config/index.js
+++ app/config/index.js
@@ -3,6 +3,8 @@
  * Module dependencies.
  */
 
+var _ = require('lodash');
+var config = require('evergram-common').config;
 var devConfig = require('./env/development');
 var testConfig = require('./env/test');
 var prodConfig = require('./env/production');
@@ -12,11 +14,13 @@ var prodConfig = require('./env/production');
  */
 
 function Config() {
-    return {
+    var localConfig = {
         development: devConfig,
         test: testConfig,
         production: prodConfig
     }[process.env.NODE_ENV || 'development'];
+
+    return _.merge(localConfig, config);
 }
 
 module.exports = exports = new Config();
